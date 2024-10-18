24e6c2b792168dd008e5570467f834ea2981adf3
jash.liao <jash.liao@gmail.com>
2024-10-18 14:26:38 +0800

    Modified 202410181426

--- Code/AvaloniaMVVM/AvaloniaMVVM/Views/MainWindow.axaml.cs
+++ Code/AvaloniaMVVM/AvaloniaMVVM/Views/MainWindow.axaml.cs
@@ -1,18 +1,130 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Threading;
+using System.Text;
+using Avalonia.Media;
+using System;
+using System.Threading;
+using System.Timers;
+
 
 namespace AvaloniaMVVM.Views
 {
     public partial class MainWindow : Window
     {
+        //---
+        //Thread
+        private Thread m_timerThread;
+        private bool m_running;
+        private void InitializeThread()
+        {
+            m_running = true;
+            m_timerThread = new Thread(ThreadFunction);
+            m_timerThread.Start();
+        }
+        private void ThreadStop()
+        {
+            m_running = false;
+            m_timerThread?.Join();
+        }
+
+        private void ThreadFunction()
+        {
+            while (m_running)
+            {
+                Thread.Sleep(2000);
+                Dispatcher.UIThread.InvokeAsync(() =>
+                {
+                    GreetingTextBlock.Text = DateTime.Now.ToString("HH:mm:ss");
+                });
+            }
+        }
+        //---Thread
+
+        //---
+        //Timer
+        private System.Timers.Timer m_timer;
+        private void InitializeTimer()
+        {
+            /*
+             m_timer.Start();
+             m_timer.Stop();
+            */
+            m_timer = new System.Timers.Timer(2000); // 每秒觸發一次
+            m_timer.Elapsed += OnTimedEvent;
+            m_timer.AutoReset = true;
+            m_timer.Start();
+        }
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                GreetingTextBlock.Text = DateTime.Now.ToString("HH:mm:ss");
+            });
+        }
+        //---Timer
+
         public MainWindow()
         {
             InitializeComponent();
-            this.WindowState = WindowState.Maximized;
+            FullScreenMode();
+            SpecifyScreen();
+            ShowScreenResolution();
+            InitializeThread();//InitializeTimer();
+        }
+        private void SpecifyScreen(int intIndx=1)
+        {
+            if ((intIndx - 1) < 0)
+                return;
+
+            // 獲取所有螢幕
+            var allScreens = Screens.All;
+
+            // 選擇第二個螢幕 (若有)
+            if (allScreens.Count > (intIndx-1))
+            {
+                var targetScreen = allScreens[(intIndx - 1)];
+                Position = new PixelPoint(targetScreen.WorkingArea.X, targetScreen.WorkingArea.Y);
+            }
+        }
+        private void FullScreenMode()
+        {
+            this.WindowState = WindowState.FullScreen;//WindowState.Maximized;
             //隱藏工具列
             this.ExtendClientAreaToDecorationsHint = true;
             this.ExtendClientAreaTitleBarHeightHint = -1;
             //---隱藏工具列
             this.SystemDecorations = SystemDecorations.None;
+        }
+        private void ShowScreenResolution()
+        {
+            StringBuilder resolutionInfo = new StringBuilder();
+
+            /*
+            var screens = Screens.Primary.WorkingArea;
+            var screenWidth = screens.Width;
+            var screenHeight = screens.Height;
+            resolutionInfo.AppendLine($"Primary Screen: {screenWidth} x {screenHeight}");
+            */
+
+            int i = 0;
+            foreach (var screen in Screens.All)
+            {
+                i++;
+                resolutionInfo.AppendLine($"Screen {i}: {screen.Bounds.Width} x {screen.Bounds.Height}");
+            }
+
+            Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                ResolutionTextBlock.Text = resolutionInfo.ToString();
+            });
+        }
+        private void BtnSet_Click(object sender, RoutedEventArgs e)
+        {
+            ResolutionTextBlock.FontWeight = FontWeight.Bold;
+            BtnSet.Content = "FontWeight.Bold";
+            ThreadStop();//m_timer.Stop();
         }
     }
 }
--- Readme.txt
+++ Readme.txt
@@ -25,19 +25,24 @@ Avalonia 測試項目
 	★可以切換.NET版本 OK
 	★可以編譯X64/X32版本 OK
 	★全螢幕&隱藏工具列 OK
+		private void FullScreenMode()
 	★單一空殼專案編譯速度比MAUI 快
 	★單一空殼專案編譯後產生檔案數量/複雜度/大小比MAUI 優 (X64下 38檔案數/25M)
 	★單一空殼專案啟動執行速度比MAUI 快 (明顯能區別)
 	★字型大小
 		◇字型大小設定方式
-			▼<TextBlock FontSize="20" FontFamily="Arial" FontWeight="Bold" FontStyle="Italic" Text="{Binding Greeting}" HorizontalAlignment="Center" VerticalAlignment="Center"/>
+			<TextBlock FontSize="20" FontFamily="Arial" FontWeight="Bold" FontStyle="Italic" Text="{Binding Greeting}" HorizontalAlignment="Center" VerticalAlignment="Center"/>
 				裝置無關單位 (dius): dius 是一個相對單位，其大小會根據螢幕解析度和 DPI 而變化，確保文字在不同設備上顯示比例一致。
 				單位: 雖然 FontSize 接受 double 值，但實際上你通常會使用整數值。
 				樣式優先順序: 如果一個元素同時受到樣式和直接屬性設定的影響，直接屬性設定的優先級更高。			
+	★解析度調整影響
 		◇解析度是否會直接影響字型大小
 			以下測試結果放在【解析度對文字顯示影響】資料夾中
-	★解析度調整影響
-	★動態變更配置UI版面調整大小
+	★指定顯示螢幕 可以
+		private void SpecifyScreen(int intIndx=1)
+	★Timer/Thread
+		◇System.Timers 可以
+		◇System.Threading 可以
 	★LISTVIEW 渲染效能
 		◇單一/整批 更新
 		◇改變顏色
@@ -49,11 +54,32 @@ Avalonia 測試項目
 	★彈出視窗
 		◇效能
 		◇多層疊加
-	★基本元件
+	★基本元件 [https://docs.avaloniaui.net/zh-Hans/docs/basics/user-interface/controls/builtin-controls]
+			  [https://reference.avaloniaui.net/api/Avalonia.Controls/]
+		◇ListBox 
+			https://docs.avaloniaui.net/zh-Hans/docs/reference/controls/listbox
 		◇switch
 		◇checkbox
 		◇comboBox
-		◇image
-		◇配置元件
-		◇邊框元件
+		◇Button
+			https://docs.avaloniaui.net/zh-Hans/docs/reference/controls/buttons/button
+		◇圖片元件
+			▲Image
+				https://docs.avaloniaui.net/zh-Hans/docs/reference/controls/image
+			▲image-controls
+				https://docs.avaloniaui.net/zh-Hans/docs/reference/controls/image-controls
+		◇布局元件
+			▲Panel
+				https://docs.avaloniaui.net/zh-Hans/docs/reference/controls/panel
+			▲StackPanel
+				Spacing: 間距
+				Orientation: 設定堆疊的方向，可以是 Horizontal（水平）或 Vertical（垂直）。
+				Children: 包含所有子元素的集合。
+			▲Grid	
+				https://docs.avaloniaui.net/zh-Hans/docs/reference/controls/grid/			
+			▲Border
+				https://docs.avaloniaui.net/zh-Hans/docs/reference/controls/border
 	★元件開發
+		◇圖片按鈕
+			
+	★動態變更配置UI版面調整大小	
